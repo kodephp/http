364718adcee9c3f41a365ac50e703b97be7d82ae
半本正经 <zyk96321@163.com>
2026-03-28 15:42:28 +0800

    release: v1.3.0 适配 kode/exception ^2.0

--- src/Kode.php
+++ src/Kode.php
@@ -19,7 +19,7 @@ namespace Kode\Http;
 class Kode
 {
     /** @var string 版本号 */
-    public const VERSION = '1.2.2';
+    public const VERSION = '1.3.0';
 
     /** @var array<string, mixed> 全局配置 */
     protected static array $config = [];
