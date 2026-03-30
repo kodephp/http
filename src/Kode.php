5390dea7a5da10b693a9a255a6dd1ec34b35c959
半本正经 <zyk96321@163.com>
2026-03-30 11:28:24 +0800

    release: v2.0.0 借鉴主流框架设计重构 API

--- src/Kode.php
+++ src/Kode.php
@@ -19,7 +19,7 @@ namespace Kode\Http;
 class Kode
 {
     /** @var string 版本号 */
-    public const VERSION = '1.5.0';
+    public const VERSION = '2.0.0';
 
     /** @var array<string, mixed> 全局配置 */
     protected static array $config = [];
